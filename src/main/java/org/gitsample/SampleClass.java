7eead399aba71f29cc7ae926e33409059860df89
marco.cireni <marco.cireni@accesso.com>
2025-06-19 08:12:03 +0200

    added temp, get, set, toString

--- src/main/java/org/gitsample/SampleClass.java
+++ src/main/java/org/gitsample/SampleClass.java
@@ -1,9 +1,24 @@
 package org.gitsample;
 
 public class SampleClass {
+  private String temp;
+
+  public String getTemp() {
+    return temp;
+  }
+
+  @Override
+  public String toString() {
+    return "SampleClass [temp=" + temp + "]";
+  }
+
+  public void setTemp(String temp) {
+    this.temp = temp;
+  }
 
   public static void main(String[] args) {
     //changes made from git hub
   }
+
 
 }
