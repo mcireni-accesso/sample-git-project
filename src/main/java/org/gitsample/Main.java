d29cbe9594af792e26b0ee2fb894225785d309f9
Marco Cireni <143717236+mcireni-accesso@users.noreply.github.com>
2025-06-19 09:53:31 +0200

    Update Main.java
    System.out.println("Conflict - feature branch");

--- src/main/java/org/gitsample/Main.java
+++ src/main/java/org/gitsample/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
   public static void main(String[] args) {
     System.out.println("Test main");
+    System.out.println("Conflict - feature branch");
     //modification made in feature branch SIT-15960
   }
 
