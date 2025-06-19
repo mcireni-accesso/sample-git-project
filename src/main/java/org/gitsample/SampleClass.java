eb97c6aaca566a345f2f90a62598ff4c17c41c91
Marco Cireni <143717236+mcireni-accesso@users.noreply.github.com>
2025-06-19 09:54:13 +0200

    Update SampleClass.java
    added System.out.println("Conflict - feature branch - sample class");

--- src/main/java/org/gitsample/SampleClass.java
+++ src/main/java/org/gitsample/SampleClass.java
@@ -17,6 +17,7 @@ public class SampleClass {
   }
 
   public static void main(String[] args) {
+    System.out.println("Conflict - feature branch - sample class");
     //changes made from git hub
   }
 
